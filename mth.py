c522d0206622c35f30780079a41879075136ba11
ntzex <ntzex>
2022-12-07 17:51:51 +0300

    Init zalupa

---
+++ mth.py
@@ -0,0 +1,17 @@
+from math import pi
+
+
+def square_area(a):
+        """Calculate cube area"""
+        return pow(a, 2)
+
+
+def rect_area(a, b):
+        """Calculate rectangle area"""
+        return a*b
+
+
+def circle_area(r):
+        """Calculate circle area"""
+        return pi*pow(r, 2)
+
