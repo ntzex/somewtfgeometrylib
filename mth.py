153e85449ba26647798e2a4610fa1b29af1217a3
ntzex <ntzex>
2022-12-07 20:38:27 +0300

    PEP8 GOES BRRRRRRRRRR

--- mth.py
+++ mth.py
@@ -1,30 +1,31 @@
 from math import pi
 
-# -- AREA -- #
+
 def square_area(a):
-        """Calculate cube area"""
-        return pow(a, 2)
+    """Calculate cube area (a^2)"""
+    return pow(a, 2)
 
 
 def rect_area(a, b):
-        """Calculate rectangle area"""
-        return a*b
+    """Calculate rectangle area (a*b)"""
+    return a*b
 
 
 def circle_area(r):
-        """Calculate circle area"""
-        return pi*pow(r, 2)
+    """Calculate circle area (pi*r^2)"""
+    return pi*pow(r, 2)
 
-
-# -- PERIMETER -- #
 
 def square_perimeter(a):
-        """Calculate square perimeter"""
-        return a*2
+    """Calculate square perimeter (a*2)"""
+    return a*2
 
 
 def rect_perimeter(a, b):
-        return a+b
+    """Calculate rectangle perimeter (a+b)"""
+    return a+b
 
 
-def circle_perimeter(r): ... # Вроде 2*pi*r
+def circle_perimeter(r):
+    """Calculate circle perimeter (2*pi*r)"""
+    return 2*pi*r
